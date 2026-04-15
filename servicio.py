1f4a8d9bea02d21a363204a2df68c4c4366cce1f
Thais <thaismedina@mail.weber.edu>
2026-04-15 15:08:13 -0700

    Update servicio.py

--- servicio.py
+++ servicio.py
@@ -37,6 +37,7 @@ class ServicioTecnico:
     def menu(self):
         print("\n=== Servicio Técnico Pro ===")
         print(f"[Servicio Técnico]: Bienvenido {self.cliente}")
+        print(f"[Servicio Técnico]: {self.cliente}, seleccione una opción")
         print("1️⃣ Revisar")
         print("2️⃣ Reparar")
         print("3️⃣ Donar")
